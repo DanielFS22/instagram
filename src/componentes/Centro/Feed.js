ceb39fa01b6fcecdae356ad44531fd2190f6d75c
DanielFS22 <161477598+DanielFS22@users.noreply.github.com>
2024-05-09 10:16:52 -0300

    Update Feed.js

--- src/componentes/Centro/Feed.js
+++ src/componentes/Centro/Feed.js
@@ -1,32 +1,94 @@
 import './Feed.css';
-
 import Post from './Post/Post';
 
 export default function Feed() {
     return (
         <div className="Feed">
-
             <Post
                 title="Neymarjr"
                 imgSrc="https://fly.metropoles.cloud/upload/q_85,w_600/https://uploads.metropoles.cloud/wp-content/uploads/2023/03/07094558/GettyImages-1390440335.jpg"
                 body="Another important win! 💪" />
+            <div className="Comments">
+                <h4>Ver todos os 23 comentários</h4>
+                <p><strong>brunoagnello</strong> The best in the world 🌎 🔥🔥🔥🔥👏👏👏👏</p>
+                <p> <strong>luizpiraju</strong> Vamooos 👏👏👏</p>
+                <input type='text' placeholder='Adicione um comentario' />
+            </div>
             <Post
                 title="brunamarquezine"
-                imgSrc="https://ofuxico.com.br/img/galeria/2015/03/259235.jpg"
-                body="fire 🔥" />
+                imgSrc="https://s2-quem.glbimg.com/-39_oFjg86-w1yBY_QMKDNhjE_c=/0x32:1080x819/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_b0f0e84207c948ab8b8777be5a6a4395/internal_photos/bs/2024/f/B/PQdBGhQUawNEiC6nCdeA/snapinsta.app-428134172-18461394544007479-7547756283616809202-n-1080.jpg"
+                body="eu não consigo pensar em uma legenda que não seja ÓÓÓÓ BEBÊ É A VEVETA QUE TÁ NO COMANDO" />
+            <div className="Comments">
+                <h4>Ver todos os 4.323 comentários</h4>
+                <p><strong>victtorfernando </strong> ❤️❤️❤️</p>
+                <p> <strong>sandyoficcial</strong>😍</p>
+                <input type='text' placeholder='Adicione um comentario' />
+            </div>
 
             <Post
                 title="brunabiancardi"
                 imgSrc="https://s2-gshow.glbimg.com/Zh1BR3Oq_ZDr4eOJGjG3-g-3zsg=/0x0:1440x1440/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_e84042ef78cb4708aeebdf1c68c6cbd6/internal_photos/bs/2022/C/x/uqhrn4Sr2fjLAbvKo8CA/efeito-neymar-namorada-do-craque-bruna-biancardi-tem-aumento-de-500-em-numero-de-seguidores-nas-redes.jpg"
                 body="i love u my boy friend" />
+            <div className="Comments">
+                <h4>Ver todos os 46 comentários</h4>
+                <p><strong>rasardinha</strong> A paisagem no caso é você, né? 😍</p>
+                <p> <strong>sahbiaa</strong> 😍😍😍</p>
+                <input type='text' placeholder='Adicione um comentario' />
+            </div>
             <Post
                 title="ochicoin"
                 imgSrc="https://ofuxico.com.br/wp-content/uploads/2023/11/chico-moedas-muda-visual.jpg"
                 body="valeu po 👍🏻" />
+            <div className="Comments">
+                <h4>Ver todos os 556 comentários</h4>
+                <p><strong>eurodriguezzz_</strong> Ninguém: A  // EU: EU TENTEEEII</p>
+                <p> <strong>pigzadaxd</strong> não sei se te amo ou te odeio</p>
+                <input type='text' placeholder='Adicione um comentario' />
+            </div>
             <Post
                 title="luisasonza"
                 imgSrc="https://portalpopline.com.br/wp-content/uploads/2024/04/luisa-sonza-post-enigmatico-escandalo-intimo-jpg.webp"
                 body="strawberry fields 🍓" />
+            <div className="Comments">
+                <h4>Ver todos os 2.167 comentários</h4>
+                <p><strong>camilamoura225</strong> Maravilhosaa ❤️</p>
+                <p> <strong>fernandorocha</strong> 🤘❤️‍🔥</p>
+                <input type='text' placeholder='Adicione um comentario' />
+            </div>
+
+            <Post
+                title="whinderssonnunes"
+                imgSrc="https://images.redetv.uol.com.br/public/programas/tvfama/20231201163806w8ETTlcWUl.jpeg"
+                body="Eu não costumo receber prêmios, e eles param de me dar pq eu não busco, eles mandam o convite então eles tem meu endereço kkkkkkk" />
+            <div className="Comments">
+                <h4>Ver todos os 26,7 mil comentários</h4>
+                <p><strong>diogodefante</strong> lindo ❤️</p>
+                <p> <strong>lucaslucco</strong> 😍😍😍😍😍</p>
+                <input type='text' placeholder='Adicione um comentario' />
+            </div>
+
+            <Post
+                title="shakira"
+                imgSrc="https://www.metroworldnews.com.br/resizer/v2/GXGD6J3DTFFZZGNO2DGLEG27EM.png?smart=true&auth=5676d6e8e50e3e9e02cbd68527ee4bfdbdef27074ca0c7622494ad7886101cc0&width=992&height=558"
+                body="Museu Metropolitano de Arte, Nova York" />
+            <div className="Comments">
+                <h4>Ver todos os 7.047 comentários</h4>
+                <p><strong>nicolasgeradin</strong> Nossa eterna rainha ❤️❤️❤️</p>
+                <p> <strong>deborahdosfalsetes</strong> Naturalmente linda</p>
+                <input type='text' placeholder='Adicione um comentario' />
+            </div>
+
+            <Post
+                title="3gerardpique"
+                imgSrc="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/03/27/502470060-xvvsikn3zrf2hlwxzi25vomd6a.jpeg"
+                body="❤️" />
+            <div className="Comments">
+                <h4>Ver todos os 874 mil comentários</h4>
+                <p><strong>mahbobeh.rezaii</strong> 👎👎👎🤢🤢</p>
+                <p> <strong>somayeh.ahmdian</strong> 🤢🤢🤮</p>
+                <input type='text' placeholder='Adicione um comentario' />
+            </div>
         </div>
     )
 }
+{/**/ }
