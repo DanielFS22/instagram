402a1216a74bc15ff508c4575eea78fa1ca45279
DanielFS22 <161477598+DanielFS22@users.noreply.github.com>
2024-05-09 10:18:50 -0300

    Update ConteudoLateralEsquerda.js

--- src/componentes/Esquerda/ConteudoLateralEsquerda.js
+++ src/componentes/Esquerda/ConteudoLateralEsquerda.js
@@ -7,14 +7,16 @@ import { IoSearch } from "react-icons/io5";
 import { GoHeart } from "react-icons/go";
 import { CgAddR } from "react-icons/cg";
 import { TbBrandMessenger } from "react-icons/tb";
+import { FaUserAlt } from "react-icons/fa";
 import { IoIosArrowDown } from "react-icons/io";
-import { CgProfile } from "react-icons/cg";
+import { FaThreads } from "react-icons/fa6";
+import { IoMenu } from "react-icons/io5";
 
 export default function ConteudoLateralEsquerda() {
     return (
         <div className='conteudoLateralEsquerda'>
             <img src={logo} alt='Logo' />
-            
+             
             <ul>
                 <Menu nome="Home" icone1={<IoIosArrowDown/>} classe = "text-menu" icone={<IoMdHome/>} />
 
@@ -26,8 +28,13 @@ export default function ConteudoLateralEsquerda() {
                 
                 <Menu nome = "Create" classe = "text-menu" icone = {<CgAddR/>}/>
 
-                <Menu className='Profile' nome = "Profile" classe = "text-menu" icone = {<CgProfile />}/>
+                <Menu className='Profile' nome = "Profile" classe = "text-menu" icone = {<FaUserAlt/>}/>
 
+                <div className='itensInferiores'>
+                <Menu className='threads' nome = "threads" classe = "text-menu" icone = {<FaThreads/>}/>
+                
+                <Menu className='Mais' nome = "Mais" classe = "text-menu" icone = {< IoMenu/>}/>
+                </div>
             </ul>
         </div>
     )
